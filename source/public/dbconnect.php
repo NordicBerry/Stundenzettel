06f247c08be4f6f963271ce2fd831c9064504d9c
Anna Schweikert <A_SCHWEIKERT@PGASNH.LOCAL>
2017-11-05 14:15:46 +0100

    removed default parameter for columns

--- source/public/dbconnect.php
+++ source/public/dbconnect.php
@@ -10,11 +10,18 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
+    //wenn erstes mal dann alles anlegen
+
+
     die("Connection failed: " . $conn->connect_error);
+}else {
+  echo " alles gut"
 }
 
 // Create db if none exists
 $createDb = "CREATE DATABASE IF NOT EXISTS $dbname";
+
+
 
 if(!$conn->query($createDb)) {
     echo "Error: " . $createDb . "<br>" . $conn->error;
@@ -26,12 +33,12 @@ mysqli_select_db($conn, $dbname);
 // Create table if none exists
 $createTable = "CREATE TABLE IF NOT EXISTS `stundenzettel` (
   `id` int(11) unsigned NOT NULL auto_increment,
-  `mitarbeiter` varchar(255) NOT NULL default '',
-  `datum` date NOT NULL DEFAULT '',
-  `beschreibung` varchar(255) NOT NULL DEFAULT '',
-  `projektnummer` int(11) NOT NULL DEFAULT '',
-  `uhrzeitvon` time NOT NULL DEFAULT '',
-  `uhrzeitbis` time NOT NULL DEFAULT '',
+  `mitarbeiter` varchar(255) NOT NULL,
+  `datum` date NOT NULL DEFAULT ,
+  `beschreibung` varchar(255) NOT NULL,
+  `projektnummer` int(11) NOT NULL ,
+  `uhrzeitvon` time NOT NULL,
+  `uhrzeitbis` time NOT NULL,
   PRIMARY KEY (`id`)
 )";
 
